357b8340b58122a1d9fcc405bbfcdb9788dcf39e
VadimThink <54996709+VadimThink@users.noreply.github.com>
2021-10-22 02:01:17 +0300

    I wonder to make a lot of commits but forgot) So we have only one

---
+++ Faker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Generator;
+using DTO;
+using System.Reflection;
+
+namespace Faker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseGenerator baseGenerator = new BaseGenerator();
+            baseGenerator.AddGenerator(new SongsGenerator.SongsGenerator());
+            baseGenerator.AddGenerator(new UriGenerator());
+
+            Assembly asm =
+                Assembly.LoadFrom("../../../../PrimetiveGenerators/bin/Debug/PrimetiveGenerators.dll");
+            var types = asm.GetTypes();
+            foreach (var type in types)
+            {
+                if (!type.IsAbstract && type.GetInterface("IBaseGenerator") != null)
+                {
+                    baseGenerator.AddGenerator((IBaseGenerator)Activator.CreateInstance(type));
+                }
+            }
+
+            IGenerator generator = new DTOGenerator(baseGenerator);
+            var book = generator.Generate(typeof(Book));
+            Console.WriteLine(book.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SongsGenerator/SongsGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Generator;
+using System.IO;
+
+namespace SongsGenerator
+{
+    public class SongsGenerator : IBaseGenerator
+    {
+        private const string PATH_TO_TEXT = "songs.txt";
+        private string[] lines;
+        private Random random;
+        public Type GeneratingType
+        {
+            get { return typeof(string); }
+        }
+        public SongsGenerator()
+        {
+            string tempText = File.ReadAllText(PATH_TO_TEXT);
+            lines = tempText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            random = new Random();
+        }
+
+
+        public object Generate()
+        {
+            return lines[random.Next(lines.Length)];
+        }
+    }
+}
